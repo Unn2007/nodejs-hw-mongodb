60b3dc454d879b94b5f145cf9b6385ad32e38541
Unn2007 <unn2007@i.ua>
2024-10-07 15:45:48 +0300

    upd

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,33 +1,26 @@
 import { getAllContacts, getContactById } from '../services/contacts.js';
-export const getContactsController  = async (req, res) => {
-    const contacts = await getAllContacts();
+import createHttpError from 'http-errors';
 
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
+export const getContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
 
-    });
-  };
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
 
-  export const getContactsIdController = async (req, res) => {
-    const { contactId } = req.params;
-
-  try {
-    const contact = await getContactById(contactId);
-    if (contact) {
-      res.status(200).json({
-        status: 200,
-        message: `Successfully found contact with id ${contactId}!`,
-        data: contact,
-      });
-    }
-  } catch (error) {
-    res.status(404).json({
-      message: 'Contact not found',
-    });
+export const getContactsIdController = async (req, res) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
   }
-  };
-  
-  
-  
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { getContactsController, getContactsIdController } from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 const contactsRouter = Router();
 
-contactsRouter.get('/contacts', getContactsController);
+contactsRouter.get('/contacts', ctrlWrapper(getContactsController));
 
-contactsRouter.get('/contacts/:contactId', getContactsIdController);
+contactsRouter.get('/contacts/:contactId', ctrlWrapper(getContactsIdController));
 
 export default contactsRouter;
