0602a2c8820a526771e6fac0847c485fbfdae0f0
Unn2007 <unn2007@i.ua>
2024-10-24 16:51:43 +0300

    upd

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -6,6 +6,7 @@ export const createContactSchema = Joi.object({
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
+  userId: Joi.string().required(),
 });
 
 export const updateContactSchema = Joi.object({
