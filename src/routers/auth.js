957d63e6c81953494abdcaa1b84d5bbd8394b495
Unn2007 <unn2007@i.ua>
2024-10-30 10:39:18 +0200

    upd

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -11,6 +11,12 @@ import { loginUserSchema } from '../validation/auth.js';
 
 import { validateBody } from '../middlewares/validateBody.js';
 
+import { requestResetEmailSchema } from '../validation/auth.js';
+import { requestResetEmailController } from '../controllers/auth.js';
+import { resetPasswordSchema } from '../validation/auth.js';
+import { resetPasswordController } from '../controllers/auth.js';
+
+
 const router = Router();
 
 router.post(
@@ -28,5 +34,17 @@ router.post(
 router.post('/logout', ctrlWrapper(logoutUserController));
 
 router.post('/refresh', ctrlWrapper(refreshUserSessionController));
+
+router.post(
+  '/request-reset-email',
+  validateBody(requestResetEmailSchema),
+  ctrlWrapper(requestResetEmailController),
+);
+
+router.post(
+  '/reset-password',
+  validateBody(resetPasswordSchema),
+  ctrlWrapper(resetPasswordController),
+);
 
 export default router;
